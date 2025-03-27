8b52a972674ee0831996ec4f7e7dde142ebdf33d
Robin Wagner <robinwagner@MacBookAir.fritz.box>
2025-03-27 21:41:24 +0100

    Added throtteling

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
+import { ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
-  imports: [ConfigModule.forRoot(), AuthModule],
+  imports: [
+    ConfigModule.forRoot(),
+    ThrottlerModule.forRoot({ throttlers: [{ ttl: 60000, limit: 3 }] }),
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,14 +1,16 @@
-import { Body, Controller, Post, Res } from '@nestjs/common';
+import { Body, Controller, Post, Res, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
 import { Response } from 'express';
 import { Public } from './decorators/public.decorator';
+import { ThrottlerGuard } from '@nestjs/throttler';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Public()
+  @UseGuards(ThrottlerGuard)
   @Post('login')
   login(@Body() loginDto: LoginDto, @Res() response: Response) {
     return this.authService.login(response, loginDto.password);
